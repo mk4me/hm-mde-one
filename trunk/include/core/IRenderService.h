390f360a0a02a0abb9ad97105f989226ede64c86
mszynalik <mszynalik@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2010-11-25 12:10:53 +0000

    dodanie brakujacych plików w poprzednim commicie
    git-svn-id: https://localhost:8443/svn/hm-mde-1@153 fca3399c-2208-499e-aaa1-2cf7fb07c25e

---
+++ trunk/include/core/IRenderService.h
@@ -0,0 +1,56 @@
+#ifndef I_RENDER_SERVICE_H
+#define I_RENDER_SERVICE_H
+
+
+#include <osgViewer/GraphicsWindow>
+#include <osgViewer/View>
+
+#include <core/QOSGWidget.h>
+
+
+class IC3DModel;
+class Mesh;
+class IModel;
+
+//--------------------------------------------------------------------------------------------------
+// Render Service
+//--------------------------------------------------------------------------------------------------
+
+typedef osg::ref_ptr<osg::Group> osgGroupPtr;
+
+class IRenderService: public IService
+{
+public:
+    virtual ~IRenderService() {};
+
+    virtual void TestScreenForNewModel(IModel* model) = 0;
+    virtual void AddObjectToRender(osg::Node* node) = 0;
+    virtual void SetScene(osg::Group* root) = 0;
+    virtual void Clear() = 0;
+
+
+    virtual osgViewer::Scene* GetMainWindowScene() = 0;
+    virtual osgGroupPtr GetRoot() = 0;
+    virtual QOSGViewer* GetMainAdapterWidget() = 0;
+
+    virtual osg::Light* GetLight() = 0;
+
+    virtual void RenderBone(IModel* model) = 0;       // TODO: prawdopodobnie usunac t¹metode
+    virtual void RenderC3D(IC3DModel* c3dmodel) = 0;  // TODO: prawdopodobnie usunac t¹metode
+
+    virtual void DrawNormals(IModel* model, float size) = 0; // TODO: prawdopodobnie usunac t¹metode
+
+    virtual void EnableMesh() = 0;
+    virtual void DisableMesh() = 0;
+
+    virtual void EnableBone() = 0;
+    virtual void DisableBone() = 0;
+
+    virtual void EnableMarker() = 0;
+    virtual void DisableMarker() = 0;
+
+    virtual void SetC3DMarkerToRender(IC3DModel *c3dmarker) = 0;
+};
+
+
+#endif //I_RENDER_SERVICE_H
