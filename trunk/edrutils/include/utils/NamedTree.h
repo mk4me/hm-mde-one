38fe72a6c060189914a5f85b754ae385d8788109
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2011-03-21 20:58:18 +0000

    klasa do opisu hierarchi danych oraz przechowywania dowolnych danych w smart pointerach z uwzglednieniem ptrpolicy
    git-svn-id: https://localhost:8443/svn/hm-mde-1@360 fca3399c-2208-499e-aaa1-2cf7fb07c25e

---
+++ trunk/edrutils/include/utils/NamedTree.h
@@ -0,0 +1,305 @@
+/********************************************************************
+    created:  2011/03/17
+    created:  17:3:2011   18:15
+    filename: NamedTree.h
+    author:   Mateusz Janiak
+    
+    purpose:  
+*********************************************************************/
+#ifndef HEADER_GUARD_TIMELINE__DATASTRUCTURE_H__
+#define HEADER_GUARD_TIMELINE__DATASTRUCTURE_H__
+
+#include <utils/PtrPolicyHelper.h>
+#include <utils/PtrPolicyBoost.h>
+#include <utils/Debug.h>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/tokenizer.hpp>
+#include <vector>
+
+//! forward declaration
+class NamedTreeBase;
+
+typedef boost::shared_ptr<NamedTreeBase> NamedTreeBasePtr;
+typedef boost::weak_ptr<NamedTreeBase> NamedTreeBaseWPtr;
+
+typedef boost::shared_ptr<const NamedTreeBase> NamedTreeBaseConstPtr;
+typedef boost::weak_ptr<const NamedTreeBase> NamedTreeBaseConstWPtr;
+
+class NamedTreeBase : public boost::enable_shared_from_this<NamedTreeBase>
+{
+
+public:
+    typedef std::vector<NamedTreeBaseConstPtr> ConstChildren;
+    typedef ConstChildren::const_iterator const_iterator;
+    typedef ConstChildren::size_type size_type;
+
+private:
+    //! mapa dzieci ktore mozna modyfikowac
+    typedef std::vector<NamedTreeBasePtr> Children;
+    typedef Children::iterator iterator;
+    
+    //! definicja tokenizera opartego o boost
+    typedef boost::char_separator<char> Separator;
+    typedef boost::tokenizer<Separator> Tokenizer;
+
+private:
+
+    static const Separator separator;
+
+    //! nazwa wezla
+    std::string name;
+
+    //! \return Rodzic wezla
+    NamedTreeBaseWPtr parent;
+
+    //! \return Rodzic wezla
+    NamedTreeBaseConstWPtr constParent;
+
+    //! dzieci
+    Children children;
+
+    //! dzieci z modyfikatorem const
+    ConstChildren constChildren;
+
+public:
+
+    //! Domyslny konstruktor
+    //! \param name Nazwa wezla
+    NamedTreeBase(const std::string & name = "UnnamedNode");
+
+    //! Konstruktor kopiujacy
+    //! \param child Wezel zrodlowy do skopiowania
+    //! \param deep Glebokosc kopiowania - true = glebokie, false plytkie
+    NamedTreeBase(const NamedTreeBase & child, bool deep = true);
+
+    virtual ~NamedTreeBase(void);
+
+    //! \return Nazwa wezla
+    const std::string & getName() const;
+
+    //! \param name Nazwa wezla
+    void setName(std::string name);
+
+    //! \return Pierwsze dziecko
+    const_iterator begin() const;
+
+    //! \return Koniec dzieci
+    const_iterator end() const;
+
+    //! return Rodzic aktualnego wezla
+    const NamedTreeBaseConstWPtr & getParent() const;
+
+    //! return Rodzic aktualnego wezla
+    const NamedTreeBaseWPtr & getParent();
+
+    //! \param idx Indeks dziecka
+    //! \return Dziecko o zadanym indeksie
+    NamedTreeBasePtr & getChild(size_type idx);
+
+    //! \param name Sciezka dziecka
+    //! \return Dziecko o zadanej sciezce wzgledem aktualnego wezla
+    NamedTreeBasePtr & getChild(const std::string & path);
+
+    //! \param idx Indeks dziecka
+    //! \return Dziecko o zadanym indeksie
+    const NamedTreeBaseConstPtr & getChild(size_type idx) const;
+
+    //! \param name Sciezka dziecka
+    //! \return Dziecko o zadanej sciezce wzgledem aktualnego wezla
+    const NamedTreeBaseConstPtr & getChild(const std::string & path) const;
+
+    //! return Ilosc dzieci
+    size_type size() const;
+
+    //! \param child Dziecko do dodania
+    void addChild(const NamedTreeBasePtr & child);
+
+    //! \param child Dziecka do dodania
+    //! \param idx Pozycja na ktorej chcemy dodac dziecko
+    void addChild(const NamedTreeBasePtr & child, size_type idx);
+
+    //! \param path Sciezka do wstawienia dziecka
+    void addChild(const std::string & path);
+
+    //! \param path Sciezka do wstawienia dziecka
+    //! \param idx Pozycja dziecka do wstawienia
+    void addChild(const std::string & path, size_type idx);
+
+    //! \param child Dziecko do usuniecia
+    void removeChild(const NamedTreeBasePtr & child);
+
+    //! \param idx Pozycja dziecka do usuniecia
+    void removeChild(size_type idx);
+
+    //! \param path Sciezka do usuwanego dziecka
+    void removeChild(const std::string & path);
+
+    //! \return Root aktualnej struktury
+    NamedTreeBaseWPtr getRoot();
+
+    //! \return Root aktualnej struktury
+    NamedTreeBaseConstWPtr getRoot() const;
+
+    //! \return Czy element jest lisciem
+    virtual bool isLeaf() const;
+
+    //! \return Czy element jest rootem
+    bool isRoot() const;
+
+private:
+
+    //! Konstruktor na potrzeby tworzenia hierarchi, niedostepny dla uzytkownika
+    //! \param parent Rodzic wezla - na wewnetrzne potrzeby hierarchi
+    //! \param name Nazwa wezla
+    NamedTreeBase(const NamedTreeBasePtr & parent, const std::string & name);
+
+    //! \return Pierwsze dziecko
+    iterator begin();
+
+    //! \return Koniec dzieci
+    iterator end();
+
+    //! \param name Nazwa dziecka do znalezienia
+    //! \return Iterator znalezionego dziecka lub end()
+    iterator findChildByName(const std::string & name);
+
+    //! \param name Nazwa dziecka do znalezienia
+    //! \return Iterator znalezionego dziecka lub end()
+    const_iterator findChildByName(const std::string & name) const;
+
+    //! \param path Sciezka dziecka do znalezienia
+    //! \return znalezione dziecka lub nullptr - NamedTreeBasePtr()
+    NamedTreeBasePtr findChildByPath(const std::string & path) const;
+
+    //! \param path Sciezka dziecka do znalezienia
+    //! \return znalezione dziecka lub nullptr - NamedTreeBasePtr()
+    template<class Iter>
+    NamedTreeBasePtr findLastChildInPath(Iter & begin, Iter end) const
+    {
+        UTILS_ASSERT((begin != end), "Bledna sciezka do dziecka!");
+
+        NamedTreeBaseConstPtr ret = shared_from_this();
+
+        while(begin != end) {
+            if((*begin).empty() == false){
+                if(*begin == ".."){
+                    //jesli rodzic to znaczy ze chcemy wyjsc poza hierarchie!
+                    if(ret->isRoot() == true){
+                        throw std::runtime_error("Want to go higher than root in hierarchy!");
+                    }
+
+                    //do rodzica
+                    ret = ret->getParent().lock();
+                    //ret.reset(ret->getParent().lock());
+                //aktualny poziom - ignoruj
+                }else if(*begin != ".") {
+                    //szukaj dziecka na danym poziomie
+                    auto it = ret->findChildByName(*begin);
+                    if( it == ret->end()) {
+                        break;
+                    }
+
+                    ret = *it;
+                }
+            }
+            begin++;
+        }
+
+        return boost::const_pointer_cast<NamedTreeBase>(ret);
+    }
+
+    //! \param parent Nowy rodzic aktualnej struktury danych
+    void setParent(const NamedTreeBasePtr & parent);
+
+    //! resetuje rodzica dziecka na potrzeby usuwania
+    void resetParent();
+
+    template<class Iter>
+    static Iter findChildByName(Iter begin, Iter end, const std::string & name)
+    {
+        return std::find_if(begin, end, [&](NamedTreeBaseConstPtr child){ return child->getName() == name; });
+    }
+
+};
+
+template<class Data, class PtrPolicy>
+class NamedTree : public NamedTreeBase, protected PtrPolicy
+{
+    UTILS_STATIC_ASSERT( (utils::is_ptr_policy<PtrPolicy>::value), "Pointer policy is invalid. This will cause more errors." );
+
+public:
+    //!
+    typedef PtrPolicy PtrPolicy;
+    //!
+    typedef PtrPolicy Base;
+    //! Wskaznik do danych
+    typedef typename Base::Ptr<Data>::Type Ptr;
+    typedef typename Base::Ptr<const Data>::Type ConstPtr;
+
+private:
+    //! Dane
+    Ptr data;
+
+    //! Dane z modyfikatorem const
+    ConstPtr constData;
+
+public:
+
+    //! Domyslny konstruktor
+    //! \param name Nazwa wezla
+    //! \param data Dane do przechowania w wezle, domyslnie brak danych
+    NamedTree(const std::string & name = "UnnamedNode", const Ptr & data = Ptr())
+        : NamedTreeBase(name)
+    {        
+        if(data != nullptr){
+            setData(data);
+        }else{
+            PtrPolicy::initPtr(this->data);
+            PtrPolicy::initPtr(this->constData);
+        }
+    }
+
+    //! Konstruktor kopiujacy
+    //! \param name Nazwa wezla
+    //! \param deep Czy glebokie kopiowanie
+    NamedTree(const NamedTree & child, bool deep = true)
+        : NamedTreeBase(child, deep)
+    {        
+        if(deep == true){
+            setData(new Data(*(child.data)));
+        }else{
+            setData(child.data);
+        }
+    }
+
+    ~NamedTree()
+    {
+        PtrPolicy::setPtr<Data>(data, nullptr);
+        PtrPolicy::setPtr<Data>(constData, nullptr);
+    }
+
+    //! \return dane
+    const Ptr & getData()
+    {
+        return data;
+    }
+
+    //! return data
+    const ConstPtr & getData() const
+    {
+        return constData;
+    }
+
+    //! \param data Dane do przechowania w wezle
+    void setData(const Ptr & data)
+    {
+        PtrPolicy::setPtr(this->data, data);
+        PtrPolicy::setPtr(this->constData, data);
+    }
+};
+
+
+
+#endif  // HEADER_GUARD_TIMELINE__DATASTRUCTURE_H__
---
+++ trunk/edrutils/src/utils/NamedTree.cpp
@@ -0,0 +1,332 @@
+#include <utils/NamedTree.h>
+#include <utils/Debug.h>
+
+const NamedTreeBase::Separator NamedTreeBase::separator("/");
+
+NamedTreeBase::NamedTreeBase(const std::string & name)
+    : name(name)
+{
+
+}
+
+NamedTreeBase::NamedTreeBase(const NamedTreeBase & child, bool deep)
+    : name(child.name)
+{
+    //zawsze po kopii mamy roota
+    if(deep == false){
+        for(auto it = child.children.begin(); it != child.children.end(); it++){            
+            //plytkie kopiowanie
+            addChild(*it);
+        }
+    }else{
+        for(auto it = child.children.begin(); it != child.children.end(); it++){
+            //glebokie kopiowanie
+            NamedTreeBasePtr newChild(new NamedTreeBase(**it,true));
+            addChild(newChild);
+        }        
+    }
+}
+
+NamedTreeBase::NamedTreeBase(const NamedTreeBasePtr & parent, const std::string & name)
+    : parent(parent), name(name)
+{
+    UTILS_ASSERT((parent != nullptr), "Nieprawidlowy rodzic aktualnego wezla");
+}
+
+NamedTreeBase::~NamedTreeBase(void)
+{
+
+}
+
+//NamedTreeBasePtr NamedTreeBase::copy() const
+//{
+//    return NamedTreeBasePtr(new NamedTreeBase(this->getName()));
+//}
+//
+//NamedTreeBasePtr NamedTreeBase::clone() const
+//{
+//    NamedTreeBasePtr ret(copy());
+//
+//    auto it = begin();
+//    while(it != end()) {
+//        ret->addChild((*it)->clone());
+//        it++;
+//    }
+//
+//    return ret;
+//}
+
+NamedTreeBasePtr NamedTreeBase::findChildByPath(const std::string & path) const
+{
+    UTILS_ASSERT((path.empty() == false), "Bledna sciezka do dziecka!");
+
+    Tokenizer tok(path, separator);
+
+    auto it = tok.begin();
+
+    NamedTreeBasePtr ret = findLastChildInPath(it, tok.end());
+
+    if(it != tok.end()) {
+        ret.reset();
+    }
+
+    return ret;
+}
+
+const std::string & NamedTreeBase::getName() const
+{ 
+    return name;
+}
+
+void NamedTreeBase::setName(std::string name) 
+{ 
+    this->name = name; 
+}
+
+
+const NamedTreeBaseWPtr & NamedTreeBase::getParent()
+{ 
+    return this->parent;
+}
+
+const NamedTreeBaseConstWPtr & NamedTreeBase::getParent() const
+{ 
+    return this->constParent;
+}
+
+NamedTreeBase::iterator NamedTreeBase::begin()
+{
+    return children.begin();
+}
+
+NamedTreeBase::iterator NamedTreeBase::end()
+{
+    return children.end();
+}
+
+NamedTreeBasePtr & NamedTreeBase::getChild(NamedTreeBase::size_type idx)
+{
+    return children[idx];
+}
+
+NamedTreeBasePtr & NamedTreeBase::getChild(const std::string & path)
+{
+    NamedTreeBasePtr child = findChildByPath(path);
+    if(child == nullptr) {
+        throw std::runtime_error("Child does not exist!");
+    }
+
+    if(child->isRoot() == true){
+        throw std::runtime_error("Root reference must be hold by client!");
+    }
+
+    NamedTreeBasePtr parent(child->getParent().lock());
+
+    return *(parent->findChildByName(child->getName()));
+}
+
+NamedTreeBase::const_iterator NamedTreeBase::begin() const
+{
+    return constChildren.begin();
+}
+
+NamedTreeBase::const_iterator NamedTreeBase::end() const
+{
+    return constChildren.end();
+}
+
+const NamedTreeBaseConstPtr & NamedTreeBase::getChild(NamedTreeBase::size_type idx) const
+{
+    return constChildren[idx];
+}
+
+const NamedTreeBaseConstPtr & NamedTreeBase::getChild(const std::string & path) const
+{
+    NamedTreeBaseConstPtr child = findChildByPath(path);
+    if(child == nullptr) {
+        throw std::runtime_error("Child does not exist!");
+    }
+
+    if(child->isRoot() == true){
+        throw std::runtime_error("Root reference must be hold by client!");
+    }
+
+    NamedTreeBaseConstPtr parent(child->getParent());
+
+    return *(parent->findChildByName(child->getName()));
+}
+
+NamedTreeBase::size_type NamedTreeBase::size() const
+{
+    return children.size();
+}
+
+NamedTreeBase::iterator NamedTreeBase::findChildByName(const std::string & name)
+{
+    return findChildByName(children.begin(), children.end(), name);
+}
+
+NamedTreeBase::const_iterator NamedTreeBase::findChildByName(const std::string & name) const
+{
+    return findChildByName(constChildren.begin(), constChildren.end(), name);
+}
+
+
+void NamedTreeBase::addChild(const NamedTreeBasePtr & child)
+{
+    addChild(child, -1);
+}
+
+void NamedTreeBase::addChild(const std::string & path)
+{
+    addChild(path, -1);
+}
+
+void NamedTreeBase::addChild(const std::string & path, NamedTreeBase::size_type idx)
+{
+    UTILS_ASSERT((path.empty() == false), "Bledna sciezka do dziecka!");
+
+    Tokenizer tok(path, separator);
+
+    auto it = tok.begin();
+
+    NamedTreeBasePtr pos = findLastChildInPath(it, tok.end());
+    
+    if(it == tok.end()) {
+        throw std::runtime_error("Child with the given name already exist!");
+    }
+
+    ////wyznaczam ostatni poziom, bedacy nazwa dla mojego nowego wezla
+    auto itEnd = tok.begin();
+    auto itTmp = tok.begin();
+    while(++itTmp != tok.end()){
+        itEnd++;
+    }
+
+    //stworz drzewo do itEnd, a pod itEnd mamy juz nazwe naszego wezsla
+    while(it != itEnd){
+        NamedTreeBasePtr nodeChild(new NamedTreeBase(*it));
+        pos->addChild(nodeChild);
+        pos = nodeChild;
+        it++;
+    }
+
+    pos->addChild(NamedTreeBasePtr(new NamedTreeBase(*itEnd)), idx);
+}
+
+void NamedTreeBase::addChild(const NamedTreeBasePtr & child, NamedTreeBase::size_type idx)
+{
+    if(child->isRoot() == false && child->getParent().lock() != nullptr) {
+        throw std::runtime_error("Child has already a parent!");
+    }
+
+    if(std::find(children.begin(), children.end(), child) != children.end()) {
+        throw std::runtime_error("Child already exist!");
+    }
+
+    if(findChildByName(child->getName()) != children.end()) {
+        throw std::runtime_error("Child with the given name already exist!");
+    }
+
+    child->setParent(shared_from_this());
+    if(idx == size_type(-1)){
+        children.push_back(child);
+        constChildren.push_back(child);
+    }else {
+        children[idx] = child;
+        constChildren[idx] = child;
+    }
+}
+
+void NamedTreeBase::removeChild(const NamedTreeBasePtr & child)
+{
+    auto it = std::remove( begin(), end(), child );
+    if ( it != end() ) {
+        //resetuj rodzica dziecka
+        //moge to zrobic, gdyz jest jeszcze minimum jedna referencja do tego obiektu w const children!
+        child->resetParent();
+        children.resize( std::distance(begin(), it) );
+        // usun element z dzieci const
+        constChildren.resize(std::distance(constChildren.begin(), std::remove( constChildren.begin(), constChildren.end(), child)));
+    }else{
+        throw std::runtime_error("Child does not exist!"); 
+    }
+}
+
+void NamedTreeBase::removeChild(NamedTreeBase::size_type idx)
+{
+    auto constIt = constChildren.begin();
+    std::advance(constIt, idx);
+
+    constChildren.erase(constIt);
+    
+    auto it = begin();
+    std::advance(it, idx);
+    (*it)->resetParent();
+    children.erase(it);
+}
+
+void NamedTreeBase::removeChild(const std::string & path)
+{
+    UTILS_ASSERT((path.empty() == false), "Bledna sciezka do dziecka!");
+
+    Tokenizer tok(path, separator);
+
+    auto it = tok.begin();
+
+    NamedTreeBasePtr pos = findLastChildInPath(it, tok.end());
+
+    if(it != tok.end()) {
+        throw std::runtime_error("Given path not exist!");
+    }
+
+    if(pos->isRoot() == true){
+        throw std::runtime_error("Can not delete root - clinet code must do this explicitly!");
+    }
+
+    pos->getParent().lock()->removeChild(pos);
+}
+
+NamedTreeBaseWPtr NamedTreeBase::getRoot()
+{    
+    NamedTreeBaseWPtr ret(shared_from_this());
+
+    while(ret.lock() != nullptr && ret.lock()->isRoot() == false){
+        ret = ret.lock()->getParent().lock();
+    }
+
+    return ret;
+}
+
+NamedTreeBaseConstWPtr NamedTreeBase::getRoot() const
+{    
+    NamedTreeBaseConstWPtr ret(shared_from_this());
+
+    while(ret.lock() != nullptr && ret.lock()->isRoot() == false){
+        ret = ret.lock()->getParent().lock();
+    }
+
+    return ret;
+}
+
+bool NamedTreeBase::isLeaf() const
+{
+    return begin() == end();
+}
+
+bool NamedTreeBase::isRoot() const
+{
+    return getParent().lock() == nullptr;
+}
+
+
+void NamedTreeBase::setParent(const NamedTreeBasePtr & parent) 
+{ 
+    this->parent = parent; 
+    this->constParent = parent;
+}
+
+void NamedTreeBase::resetParent()
+{
+    setParent(NamedTreeBasePtr());
+}
