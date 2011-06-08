9b23b732042ab60a3565ad2ed21380c3192030f2
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2011-06-08 11:37:31 +0000

    Dodanie klasy Synchronized do utils
    git-svn-id: https://localhost:8443/svn/hm-mde-1@466 fca3399c-2208-499e-aaa1-2cf7fb07c25e

---
+++ trunk/edrutils/include/utils/Synchronized.h
@@ -0,0 +1,78 @@
+/********************************************************************
+    created:  2011/05/12
+    created:  12:5:2011   15:21
+    filename: Synchronized.h
+    author:   Mateusz Janiak
+    
+    purpose:  Klasa do zapewniania synchronizacji na poziomie obiektów.
+                By wykonaæ operacje na obiekcie nale¿y wykonaæ na nim LOCK(x) { blok instrukcji }
+                lub pos³u¿yc siê typem lokuj¹cym dany obiekt na czas swojego ¿ycia - _Locker
+
+                Koncepcja pochodzi ze storny: http://tlzprgmr.wordpress.com/2008/08/10/adding-a-lock-statement-to-c/
+*********************************************************************/
+#ifndef HEADER_GUARD_UTILS__SYNCHRONIZED_H__
+#define HEADER_GUARD_UTILS__SYNCHRONIZED_H__
+
+#include <boost/type_traits/is_scalar.hpp>
+#include <boost/thread/mutex.hpp>
+
+namespace utils 
+{
+    template<class T, bool>
+    class SynchronizedImpl : public T
+    {
+    public:
+        SynchronizedImpl() : T() { }
+        SynchronizedImpl(const T &x) : T(x) { }
+    };
+
+    template<typename T>
+    class SynchronizedImpl<T, true>
+    {
+    public:
+        SynchronizedImpl() { }
+        SynchronizedImpl(const T &x) : x_(x) { }
+        operator T &() { return x_; }
+        operator const T &() const { return x_; }
+        SynchronizedImpl &operator= (T x) { x_ = x; return *this; }
+
+    private:
+        T x_;
+    };
+
+    template<typename T>
+    class Synchronized :
+        public SynchronizedImpl<T, boost::is_scalar<T>::value>, public boost::mutex
+    {
+    private:
+        typedef SynchronizedImpl<T, boost::is_scalar<T>::value> base;
+
+    public:
+        Synchronized() : base(), boost::mutex() { }
+        explicit Synchronized(const T &x) : base(x), boost::mutex() { }
+
+        Synchronized &operator= (const T &rhs)
+        {
+            base &b = *this;
+            b = rhs;
+            return *this;
+        }
+    };
+
+    class _Locker {
+    public:
+        _Locker(boost::mutex& m) : m_(m) { m_.lock(); run_ = true; }
+        _Locker(boost::mutex& m, int) : m_(m) { run_ = m_.try_lock(); }
+        ~_Locker() { m_.unlock(); }
+        bool _run() { return run_; }
+        void _stop() { run_ = false; }
+    private:
+        boost::mutex&   m_;
+        bool            run_;
+    };
+
+    #define LOCK(x) for (_Locker _lock(x); _lock._run(); _lock._stop())
+    #define TRY_LOCK(x) for (_Locker _lock(x, 0); _lock._run(); _lock._stop())
+}
+
+#endif  //  HEADER_GUARD_UTILS__SYNCHRONIZED_H__
