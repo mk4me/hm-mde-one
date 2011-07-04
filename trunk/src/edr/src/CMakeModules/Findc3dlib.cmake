b6c6150aa9c218e791351df44418cc345b45264e
wojciech.kniec <wojciech.kniec@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2011-07-04 05:47:42 +0000

    poprawki
    git-svn-id: https://localhost:8443/svn/hm-mde-1@501 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/CMakeModules/Findc3dlib.cmake
+++ trunk/src/edr/src/CMakeModules/Findc3dlib.cmake
@@ -1,6 +1,6 @@
 # przygotowanie do szukania
-FIND_INIT(C3DLIB c3dlib)
-FIND_INCLUDE_PLATFORM_HEADERS(C3DLIB c3dlib)
+FIND_INIT2(C3DLIB c3dlib "${PROJECT_INCLUDE_ROOT}/../../edrutils/include" "../../../_out/edrutils/bin/Debug" "../../../_out/edrutils/bin/Release")
+FIND_INCLUDE_PLATFORM_HEADERS2(C3DLIB c3dlib "${PROJECT_INCLUDE_ROOT}/../../../_out/edrutils")
 
 # szukanie
 FIND_SHARED(C3DLIB "c3dlib" "c3dlib")
