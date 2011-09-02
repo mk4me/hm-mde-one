c40f6f93608febfb5079f7ebe64b268e02d4a77d
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2011-09-02 10:21:51 +0000

    Usunięcie starego timeline
    git-svn-id: https://localhost:8443/svn/hm-mde-1@534 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/plugins/timeline/TimelinePlugin.cpp
+++
@@ -1,8 +0,0 @@
-#include "TimelinePCH.h"
-#include <core/Plugin.h>
-#include <plugins/timeline/TimelineService.h>
-#include <core/ILog.h>
-
-CORE_PLUGIN_BEGIN("timeline", UID::GenerateUniqueID("{DF1BD93D-1C14-4461-8E2B-1934486B98A3}"))
-CORE_PLUGIN_ADD_SERVICE(TimelineService)
-CORE_PLUGIN_END
