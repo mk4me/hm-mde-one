5270e124f12b49b5bdcc8f35565a4ac70f8057ed
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2011-09-05 11:26:25 +0000

    Aktualizacja fixa dla ostatniej rewizji wsdlpulla
    git-svn-id: https://localhost:8443/svn/hm-mde-1@537 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/plugins/communication/CommunicationService.cpp
+++ trunk/src/edr/src/plugins/communication/CommunicationService.cpp
@@ -5,7 +5,7 @@
 CommunicationService::CommunicationService()
     : name("Communication")
 {
-    XmlUtils::setDefaultUriFilesPath(core::getPathInterface()->getApplicationDataPath().string());
+    XmlUtils::TMPFILESDIR = core::getPathInterface()->getApplicationDataPath().string();
     WsdlPull::SCHEMADIR = (core::getPathInterface()->getResourcesPath() / "schemas/").string();   
     this->model = communication::CommunicationManager::getInstance();
 
