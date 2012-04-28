d6fc0541240235d74491fa75eba67eb819cb819e
wojciech.kniec <wojciech.kniec@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2012-04-28 10:14:33 +0000

    usuniecie odwolan do prywatnych naglowkow
    git-svn-id: https://localhost:8443/svn/hm-mde-1@693 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/include/plugins/kinematic/Wrappers.h
+++ trunk/src/edr/include/plugins/kinematic/Wrappers.h
@@ -4,13 +4,11 @@
 #include <core/ObjectWrapper.h>
 #include <kinematiclib/SkeletalModel.h>
 #include <kinematiclib/JointAnglesCollection.h>
-#include <plugins/kinematic/skeletalVisualizationScheme.h>
 
 CORE_DEFINE_WRAPPER(kinematic::JointAnglesCollection, utils::PtrPolicyBoost, utils::ClonePolicyVirtualCloneMethod);
 CORE_DEFINE_WRAPPER(kinematic::SkeletalData, utils::PtrPolicyBoost, utils::ClonePolicyVirtualCloneMethod);
 CORE_DEFINE_WRAPPER(kinematic::SkeletalModel, utils::PtrPolicyBoost, utils::ClonePolicyVirtualCloneMethod);
 CORE_DEFINE_WRAPPER(std::string, utils::PtrPolicyBoost, utils::ClonePolicyCopyConstructor);
-CORE_DEFINE_WRAPPER(VisualizationScheme, utils::PtrPolicyBoost, utils::ClonePolicyNotImplemented);
 
 
 #endif  // __HEADER_GUARD_KINEMATIC__WRAPPERS_H__
