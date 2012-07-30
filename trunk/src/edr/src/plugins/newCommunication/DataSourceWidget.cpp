3c0d19b1c947312996e7307960de0ee9cb4da4db
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2012-07-30 11:13:42 +0000

    przywrócenie logowania
    git-svn-id: https://localhost:8443/svn/hm-mde-1@733 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/plugins/newCommunication/DataSourceWidget.cpp
+++ trunk/src/edr/src/plugins/newCommunication/DataSourceWidget.cpp
@@ -244,10 +244,10 @@ DataSourceWidget::DataSourceWidget(CommunicationDataSource * dataSource, QWidget
 	userEdit->installEventFilter(loginEventFilter);
 	passwordEdit->installEventFilter(loginEventFilter);
 
-	onLogin("bdrdemo", ";bdrdemo");
-	setCurrentWidget(motionDataTab);
-	setTabEnabled(indexOf(configTab), false);
-	setTabEnabled(indexOf(userDataTab), false);
+	//onLogin("bdrdemo", ";bdrdemo");
+	//setCurrentWidget(motionDataTab);
+	//setTabEnabled(indexOf(configTab), false);
+	//setTabEnabled(indexOf(userDataTab), false);
 }
 
 DataSourceWidget::~DataSourceWidget()
