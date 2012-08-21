d6558496b16bb81ecec65515384f32b5339acf09
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2012-08-21 20:35:46 +0000

    poprawka dla findera sqlcipher - tak się konfiguruje i tak się buduje natywnie jako sqlite3
    git-svn-id: https://localhost:8443/svn/hm-mde-1@758 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/CMakeModules/FindSQLCipher.cmake
+++ trunk/src/edr/src/CMakeModules/FindSQLCipher.cmake
@@ -2,7 +2,7 @@
 FIND_INIT(SQLCIPHER sqlcipher)
 
 # szukanie
-FIND_SHARED(SQLCIPHER "sqlcipher" "sqlcipher")
+FIND_SHARED(SQLCIPHER "sqlite3" "sqlite3")
 add_definitions(-DSQLITE_HAS_CODEC)
 # skopiowanie
 FIND_FINISH(SQLCIPHER)
