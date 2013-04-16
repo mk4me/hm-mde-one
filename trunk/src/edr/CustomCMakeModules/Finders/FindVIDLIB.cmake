00882aa4d4ab66cc98ccdd31dc59a6d443a98149
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2013-04-16 08:21:54 +0000

    Zmiany w skryptach konfiguracyjnych w odpowiedzi na zmiany w prj-conf
    git-svn-id: https://localhost:8443/svn/hm-mde-1@881 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/CustomCMakeModules/Finders/FindVIDLIB.cmake
+++ trunk/src/edr/CustomCMakeModules/Finders/FindVIDLIB.cmake
@@ -7,7 +7,11 @@ FIND_SHARED(VIDLIB "vidlib" "vidlib")
 # skopiowanie
 FIND_FINISH(VIDLIB)
 
-if (NOT FFMPEG_FOUND)
-	message(WARNING "FFMPEG nie zostal znaleziony wiec nie mozna dodac VIDLIB")
-	set(VIDLIB_FOUND 0)
+if(LIBRARY_VIDLIB_FOUND EQUAL 1)
+	FIND_DEPENDENCIES(VIDLIB LIBRARY_VIDLIB_FOUND "FFMPEG")
 endif()
+
+#if (NOT FFMPEG_FOUND)
+#	message(WARNING "FFMPEG nie zostal znaleziony wiec nie mozna dodac VIDLIB")
+#	set(VIDLIB_FOUND 0)
+#endif()
