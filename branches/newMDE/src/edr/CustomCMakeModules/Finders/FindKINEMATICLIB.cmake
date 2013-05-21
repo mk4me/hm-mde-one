0f99f165768fb129753f6dd77f9ae263ab549f72
wojciech.kniec <wojciech.kniec@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2013-05-21 07:52:35 +0000

    git-svn-id: https://localhost:8443/svn/hm-mde-1@893 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- branches/newMDE/src/edr/CustomCMakeModules/Finders/FindKINEMATICLIB.cmake
+++
@@ -1,8 +0,0 @@
-# przygotowanie do szukania
-FIND_INIT_EDRUTILS_MODULE(KINEMATICLIB kinematiclib)
-
-# szukanie
-FIND_STATIC(KINEMATICLIB "kinematiclib")
-
-# skopiowanie
-FIND_FINISH(KINEMATICLIB)
--- branches/newMDE/src/edrutils/src/kinematiclib/src/hAnimJoint.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include <kinematiclib/hAnimJoint.h>
-
-using namespace std;
-
-namespace kinematic 
-{
-}
