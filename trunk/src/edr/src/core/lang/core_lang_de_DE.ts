17515cb65bb3e01ac3a1d68d753d6c917992f424
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2013-06-13 19:10:56 +0000

    Podniesienie minimalnej wersji CMake Usunięcie zbędnych wpisów instalacji (stare projekty)
    git-svn-id: https://localhost:8443/svn/hm-mde-1@910 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/core/lang/core_lang_de_DE.ts
+++ trunk/src/edr/src/core/lang/core_lang_de_DE.ts
@@ -4,87 +4,87 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/Application.cpp" line="168"/>
+        <location filename="../src/Application.cpp" line="182"/>
         <source>Initializing log</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="179"/>
+        <location filename="../src/Application.cpp" line="193"/>
         <source>Initializing directories</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="190"/>
+        <location filename="../src/Application.cpp" line="204"/>
         <source>Initializing translations</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="194"/>
+        <location filename="../src/Application.cpp" line="208"/>
         <source>Initializing 3D context</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="207"/>
+        <location filename="../src/Application.cpp" line="221"/>
         <source>Initializing core managers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="218"/>
+        <location filename="../src/Application.cpp" line="232"/>
         <source>Initializing plugins loader</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="241"/>
+        <location filename="../src/Application.cpp" line="255"/>
         <source>Loading plugins</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="250"/>
+        <location filename="../src/Application.cpp" line="264"/>
         <source>Initializing services</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="263"/>
+        <location filename="../src/Application.cpp" line="277"/>
         <source>Initializing sources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="270"/>
+        <location filename="../src/Application.cpp" line="284"/>
         <source>Initializing console</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="274"/>
+        <location filename="../src/Application.cpp" line="288"/>
         <source>Initializing main view</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="535"/>
+        <location filename="../src/Application.cpp" line="549"/>
         <source>Loading plugin %1 content: %2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="537"/>
+        <location filename="../src/Application.cpp" line="551"/>
         <source>domain objects</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="543"/>
+        <location filename="../src/Application.cpp" line="557"/>
         <source>services</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="549"/>
+        <location filename="../src/Application.cpp" line="563"/>
         <source>sources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="555"/>
+        <location filename="../src/Application.cpp" line="569"/>
         <source>parsers</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/Application.cpp" line="561"/>
+        <location filename="../src/Application.cpp" line="575"/>
         <source>visualizers</source>
         <translation type="unfinished"></translation>
     </message>
