0fdcba5e76d6d62611daf2de30f7bdcdddb922a0
mati_egon <mati_egon@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2014-05-07 08:29:53 +0000

    Poprawki dla MEDUSA: Dodanie komunikatów z informacją o zmianie statusu adnotacji przy wszystkich zmianach + informacja o ewentualnych błędach Dodanie dialogu z pytaniem o zatwierdzenie zmiany statusu adnotacji przy akceptacji dla lekarza (odrzucenie i prośba o recenzję realizują to poprzez dialog z komentarzem dla drugiej strony)
    git-svn-id: https://localhost:8443/svn/hm-mde-1@1078 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- trunk/src/edr/src/plugins/dicom/src/LayeredImageVisualizerView.h
+++ trunk/src/edr/src/plugins/dicom/src/LayeredImageVisualizerView.h
@@ -3,9 +3,9 @@
 	created:	17:10:2013   10:12
 	filename: 	LayeredImageVisualizerView.h
 	author:		Wojciech Kniec
-	
-	purpose:	
-*********************************************************************/
+
+	purpose:
+	*********************************************************************/
 
 #ifndef HEADER_GUARD_DICOM__LAYEREDIMAGEVISUALIZERVIEW_H__
 #define HEADER_GUARD_DICOM__LAYEREDIMAGEVISUALIZERVIEW_H__
@@ -26,70 +26,65 @@ namespace Ui { class LayeredImageVisualizer; }
 class QGraphicsPixmapItem;
 
 namespace dicom {
+	class LayeredImageVisualizer;
 
-class LayeredImageVisualizer;
+	//! Widok wizualizatora obs³uguj¹cego obrazy z warstwami
+	class LayeredImageVisualizerView : public QWidget
+	{
+		Q_OBJECT
+	public:
+		//! Standardowy konstruktor
+		LayeredImageVisualizerView(LayeredImageVisualizer* model, QWidget* parent = 0, Qt::WindowFlags f = 0);
+		virtual ~LayeredImageVisualizerView() {}
 
+	public:
+		void setAnnotationStatus(webservices::xmlWsdl::AnnotationStatus::Type status);
 
+		public Q_SLOTS:
+		//! odœwie¿enie ca³ego wizualizatora, np. gdy zmieni³a siê aktywna seria
+		void refresh();
+		//! \param enable Czy button do usuwania ma byc aktywny - dezaktywuje dla poziomu krwii i poziomu schorzenia
+		void setDeletionButtonEnabled(const bool enable);
 
-//! Widok wizualizatora obs³uguj¹cego obrazy z warstwami
-class LayeredImageVisualizerView : public QWidget
-{
-    Q_OBJECT
-public:
-    //! Standardowy konstruktor 
-    LayeredImageVisualizerView(LayeredImageVisualizer* model, QWidget* parent = 0, Qt::WindowFlags f = 0);
-	virtual ~LayeredImageVisualizerView() {}
+		private Q_SLOTS:
+		void undo();
+		void redo();
+		void crop();
+		void normalState();
+		void moveState();
 
-public:
-    void setAnnotationStatus(webservices::xmlWsdl::AnnotationStatus::Type status);
-    
-public Q_SLOTS:
-    //! odœwie¿enie ca³ego wizualizatora, np. gdy zmieni³a siê aktywna seria
-    void refresh();
-	//! \param enable Czy button do usuwania ma byc aktywny - dezaktywuje dla poziomu krwii i poziomu schorzenia
-	void setDeletionButtonEnabled(const bool enable);
-    
-private Q_SLOTS:
-    void undo();
-    void redo();
-    void crop();
-    void normalState();
-    void moveState();
+		void boneState();
+		void skinState();
+		void tendonState();
+		void jointState();
+		void inflamatoryState();
+		void noiseState();
 
-    void boneState();
-    void skinState();
-    void tendonState();
-    void jointState();
-    void inflamatoryState();
-    void noiseState();
+		void editSelectedSerie();
+		void selectionChanged(const QModelIndex &);
 
-    void editSelectedSerie();
-    void selectionChanged(const QModelIndex &);
+		void acceptAnnotation();
+		void rejectAnnotation();
+		void requestAnnotationVerification();
 
-	void acceptAnnotation();
-	void rejectAnnotation();
-	void requestAnnotationVerification();
+	private:
 
-private:
+		const bool getComment(const QString & title, const QString & content, QString & comment);
 
-	const bool getComment(const QString & title, const QString & content, QString & comment);
+		void setActionsEnabled(const bool enable);
 
-	void setActionsEnabled(const bool enable);
+		void refreshChat(const IDicomService::AnnotationStatus & as);
 
-	void refreshChat(const IDicomService::AnnotationStatus & as);
+		const bool verifySerie();
 
-	const bool verifySerie();
-
-private:
-    //! ui z designera
-    Ui::LayeredImageVisualizer* ui;
-    //! model wizualizatora
-    LayeredImageVisualizer* model;
-    QGraphicsView* lastView;
-	AdnotationsDelegate * adnotationDelegate0;
-	AdnotationsDelegate * adnotationDelegate1;
-};
-
-
+	private:
+		//! ui z designera
+		Ui::LayeredImageVisualizer* ui;
+		//! model wizualizatora
+		LayeredImageVisualizer* model;
+		QGraphicsView* lastView;
+		AdnotationsDelegate * adnotationDelegate0;
+		AdnotationsDelegate * adnotationDelegate1;
+	};
 }
 #endif
