c0a694c46ce18e809ed40073b329a35478afa921
wojciech.kniec <wojciech.kniec@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2015-01-27 20:56:20 +0000

    brakujace pliki testow
    git-svn-id: https://localhost:8443/svn/hm-mde-1@1250 fca3399c-2208-499e-aaa1-2cf7fb07c25e

---
+++ branches/kinematicRefactorForIMU/src/edrutils/tests/kinematiclib/src/SkeletonStateTest.h
@@ -0,0 +1,38 @@
+/********************************************************************
+	created:	2015/01/22
+	created:	10:44
+	filename: 	SkeletonStateTest.h
+	author:		Wojciech Kniec
+	
+	purpose:	
+*********************************************************************/
+
+#ifndef HEADER_GUARD_TEST__SkeletonStateTest_H__
+#define HEADER_GUARD_TEST__SkeletonStateTest_H__
+
+
+#include <limits>
+#include <cppunit/extensions/HelperMacros.h>
+#include <kinematiclib/SkeletonState.h>
+
+class SkeletonStateTest : public CPPUNIT_NS::TestCase
+{
+	CPPUNIT_TEST_SUITE(SkeletonStateTest);
+	CPPUNIT_TEST(testMapping);
+	CPPUNIT_TEST(testFrameConvert);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	SkeletonStateTest(void);
+	virtual ~SkeletonStateTest(void);
+	
+public:
+	void testMapping();
+	void testFrameConvert();
+	virtual void setUp();
+	virtual void tearDown();
+
+private:
+	std::unique_ptr<acclaim::Skeleton> acclaimSkeleton;
+	std::unique_ptr<acclaim::MotionData> acclaimData;
+};
+#endif
