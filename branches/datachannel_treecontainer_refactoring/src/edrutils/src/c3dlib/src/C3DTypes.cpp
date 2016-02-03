b59f448d3bd3f74fa6a33bd3f32506a5a2f967ef
wojciech.kniec <wojciech.kniec@fca3399c-2208-499e-aaa1-2cf7fb07c25e>
2016-02-03 14:09:42 +0000

    poprawka - argument generator
    git-svn-id: https://localhost:8443/svn/hm-mde-1@1433 fca3399c-2208-499e-aaa1-2cf7fb07c25e

--- branches/datachannel_treecontainer_refactoring/src/edrutils/src/c3dlib/src/C3DTypes.cpp
+++ branches/datachannel_treecontainer_refactoring/src/edrutils/src/c3dlib/src/C3DTypes.cpp
@@ -17,7 +17,7 @@ type(fromChannelNo(channelNo)), dataStart(-1.0f), dataEnd(-1.0f),
 xsignal(data.getAnalog(3 * channelNo + 0)),
 ysignal(data.getAnalog(3 * channelNo + 1)),
 zsignal(data.getAnalog(3 * channelNo + 2)),
-argumentsGenerator(1.0 / data.getPointFrequency(), data.getNumPointFrames() / data.getPointFrequency())
+argumentsGenerator(1.0 / data.getAnalogFrequency(), data.getNumAnalogFrames())
 {
 	if (xsignal == nullptr || ysignal == nullptr || zsignal == nullptr) {
 		throw std::runtime_error("Incomplete signal");
